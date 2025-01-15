bae5dae7d0d71e00099d982699fa007e8d8e1b59
username <wecp-dummy@gmail.com>
2025-01-15 11:55:24 +0000

    wecp initial commit

--- frontend/src/app/ipl/types/Match.ts
+++ frontend/src/app/ipl/types/Match.ts
@@ -1,4 +1,37 @@
 
 export class Match {
+    matchId:number;
+    firstTeamId:number;
+    secondTeamId:number;
+    matchDate: Date;
+    venue:string;
+    result:string;
+    status:string;
+    winnerTeamId:number;
+    constructor(
+        matchId:number,
+    firstTeamId:number,
+    secondTeamId:number,
+    matchDate: Date,
+    venue:string,
+    result:string,
+    status:string,
+    winnerTeamId:number,
  
+    ){
+        this.matchId=matchId;
+        this.firstTeamId=firstTeamId;
+        this.matchDate=matchDate;
+        this.secondTeamId=secondTeamId;
+        this.venue=venue;
+        this.result=result;
+        this.status=status;
+        this.winnerTeamId=winnerTeamId;
+    }
+    displayInfo(){
+    console.log(`Match ID: ${this.matchId}`);
+        console.log(`Date:${this.matchDate}`);
+        console.log(`Venue: ${this.venue}`);
+       
+    }
 }
--- frontend/src/app/ipl/types/TicketBooking.ts
+++ frontend/src/app/ipl/types/TicketBooking.ts
@@ -1,4 +1,24 @@
 
 export class TicketBooking {
+  bookingId: number;
+  email: string;
+  matchId: number;
+  numberOfTicketes: number;
 
+  constructor(
+    bookingId: number,
+    email: string,
+    matchId: number,
+    numberOfTicketes: number,
+  ){
+    this.bookingId=bookingId;
+    this.email=email;
+    this.matchId=matchId;
+    this.numberOfTicketes=numberOfTicketes;
+  }
+  displayInfo(){
+    console.log(`Booking ID: ${this.bookingId}`);
+    console.log(`Email: ${this.email}`);
+    console.log(`Number of tickets: ${this.numberOfTicketes}`);
+  }
 }
